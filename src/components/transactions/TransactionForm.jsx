b68fab3b972b9aa0d4f519ed51476d283f415479
AlexisKcc8 <CodeKJS863@gmail.com>
2023-05-10 07:48:12 -0600

    botones de seleccion ingreso-gasto añadidas

--- src/components/transactions/TransactionForm.jsx
+++ src/components/transactions/TransactionForm.jsx
@@ -7,11 +7,21 @@ let INITIAL_STATE_TRANSACTION = {
 };
 export const TransactionForm = () => {
   const [transaction, setTransaction] = useState(INITIAL_STATE_TRANSACTION);
+  const [isIncome, setIsIncome] = useState(true);
   const refInputDescription = useRef();
 
   const GLOBAL_CONTEXT = useGlobalState();
 
   const { addTransaction } = GLOBAL_CONTEXT;
+
+  const selectIncome = () => {
+    setIsIncome(true);
+  };
+
+  const selectExpense = () => {
+    setIsIncome(false);
+  };
+
   const setDataTransaction = (event) => {
     let propName = event.target.name;
     let value = event.target.value;
@@ -23,6 +33,11 @@ export const TransactionForm = () => {
 
   const submitTransaction = (e) => {
     e.preventDefault();
+    if (isIncome === false) {
+      transaction.amount = parseInt(transaction.amount) * -1;
+    } else {
+      transaction.amount = parseInt(transaction.amount);
+    }
     addTransaction(transaction);
     refInputDescription.current?.focus();
     clearInputsForm();
@@ -56,6 +71,29 @@ export const TransactionForm = () => {
           <label htmlFor="inputAmount" className="form-label">
             Monto
           </label>
+          <div className="mb-2">
+            <p>Por defecto, la opción ingreso esta seleccionada</p>
+            <div
+              className="btn fw-bold me-3"
+              style={{
+                border: "1px solid #000",
+                backgroundColor: isIncome ? "#d076ff" : "#fff",
+              }}
+              onClick={selectIncome}
+            >
+              Ingreso
+            </div>
+            <div
+              className="btn fw-bold"
+              style={{
+                border: "1px solid #000",
+                backgroundColor: isIncome ? "#fff" : "#d076ff",
+              }}
+              onClick={selectExpense}
+            >
+              Gasto
+            </div>
+          </div>
           <input
             type="number"
             onChange={setDataTransaction}
